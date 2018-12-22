dd2ccfe42068025f679cb4f7cac8cbd3c6ea8a7c
CharlyDcl <38166420+CharlyDcl@users.noreply.github.com>
2018-12-22 03:00:06 +0100

    api update

--- app/src/main/java/fr/esiea/anime/ViewModel/ItemListViewModel.java
+++ app/src/main/java/fr/esiea/anime/ViewModel/ItemListViewModel.java
@@ -1,30 +1,53 @@
 package fr.esiea.anime.ViewModel;
 
-import android.arch.lifecycle.ViewModel;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.ObservableArrayList;
 import android.databinding.ObservableField;
 import android.util.Log;
 
+import java.util.List;
+
+import fr.esiea.anime.Model.Anime.AnimeSearchResponse;
+import fr.esiea.anime.Model.Anime.ResultAnime;
+import fr.esiea.anime.Model.Api.ApiAnime;
+import fr.esiea.anime.Model.Api.ApiInterface;
 import fr.esiea.anime.View.MainActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ItemListViewModel {
 
     public ObservableField<String> screenTitle = new ObservableField<>();
     public ObservableArrayList<ItemViewModel> items = new ObservableArrayList<>();
+    public ObservableField<String> search = new ObservableField<>("");
     Context context;
-    public ItemListViewModel(Context context) {
+
+    public ItemListViewModel(final Context context) {
 
         this.context = context;
         screenTitle.set("This is a list of (x & y) where x = y * 2, click on any cell and then: y = y * 2 ");
-        for (int i = 0; i < 10; i++) {
-            //generateItem();
-        }
 
+        ReadFromApi();
     }
 
     public void ReadFromApi() {
+        ApiAnime.getRetrofitInstance().create(ApiInterface.class).getAnimesByTitle("naruto").enqueue(new Callback<ResultAnime>() {
+            @Override
+            public void onResponse(Call<ResultAnime> call, Response<ResultAnime> response) {
+                List<AnimeSearchResponse> list = response.body().getResults();
+                for (AnimeSearchResponse element : list) {
+                    items.add(items.size(), new ItemViewModel(context, element.getTitle(), element.getSynopsis(), element.getNb_episodes(), element.getScore(), element.getId()));
+                    Log.d("data api", "onResponse: " + element.getTitle());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResultAnime> call, Throwable t) {
+                Log.d("error", "onFailure: " + t.getMessage());
+            }
+        });
 
     }
 
@@ -32,9 +55,9 @@ public class ItemListViewModel {
         items.clear();
     }
 
-    public void generateItem(String title, String descr, Integer episodes, Double score) {
-        items.add(items.size(), new ItemViewModel(context, title, descr, episodes, score));
-    }
+//    public void generateItem(String title, String descr, Integer episodes, Double score, Integer id) {
+//        items.add(items.size(), new ItemViewModel(context, title, descr, episodes, score, id));
+//    }
 
     public void onItemTapped(Object item){
         Log.d("itemclick", "onItemTapped: " + ((ItemViewModel) item).description.get());
--- app/src/main/java/fr/esiea/anime/ViewModel/ItemViewModel.java
+++ app/src/main/java/fr/esiea/anime/ViewModel/ItemViewModel.java
@@ -9,18 +9,20 @@ public class ItemViewModel {
 
     public ObservableField<String> title = new ObservableField<>();
     public ObservableField<String> description = new ObservableField<>();
-    public ObservableField<Integer> episodes = new ObservableField<>();
-    public ObservableField<Double> score = new ObservableField<>();
+    public ObservableField<String> episodes = new ObservableField<>();
+    public ObservableField<String> score = new ObservableField<>();
+    public Integer id;
 
     static Random random = new Random();
     public int value;
 
-    public ItemViewModel(Context context, String title, String descr, Integer episodes, Double score){
-        assign(random.nextInt(100) + 1);
+    public ItemViewModel(Context context, String title, String descr, Integer episodes, Double score, Integer id){
+        //assign(random.nextInt(100) + 1);
         this.title.set(title);
         this.description.set(descr);
-        this.episodes.set(episodes);
-        this.score.set(score);
+        this.episodes.set(episodes.toString());
+        this.score.set(score.toString());
+        this.id = id;
     }
 
     public void squareTwo() {
